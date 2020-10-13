d9d62bfe0c3947150ae5d35cd04341ba6d03cbbe
Sofia Shanshina <lora-12574@mail.ru>
2020-10-13 15:50:00 +0700

    Files modified

--- fileforxml.py
+++ fileforxml.py
@@ -6,7 +6,7 @@ tree = ET.parse('newsafr.xml', parser)
 root = tree.getroot()
 
 
-def count_length(items):
+def count_length(items, words_len, words_top):
     description_list = list()
     for item in items:
         description_list.append(item.find('description').text)
@@ -14,34 +14,20 @@ def count_length(items):
     words_list = big_str.split(' ')
     long_words = list()
     for word in words_list:
-        if len(word) > 6:
+        if len(word) > words_len:
             long_words.append(word)
-    words_len_dict = Counter(long_words)
-    return words_len_dict
+    words_len_list = Counter(long_words).most_common(words_top)
+    return words_len_list
 
 
-def sort_list(items):
-    words_list = list()
-    for word, length in count_length(items).items():
-        word_list = [word, length]
-        words_list.append(word_list)
-    sort_el_index = 1
-
-    def sort_by_el(i):
-        return i[sort_el_index]
-    words_list = sorted(words_list, key=sort_by_el)
-    words_list = list(reversed(words_list))
-    return words_list
-
-
-def get_top_10(items):
+def get_top(items, words_len, words_top):
     result = list()
-    for i, word_list in enumerate(sort_list(items)):
-        if i < 10:
+    for i, word_list in enumerate(count_length(items, words_len, words_top)):
+        if i < words_top:
             result.append(f'{i + 1}. {word_list[0].capitalize()}: {word_list[1]}')
         final_result = '\n'.join(result)
     return final_result
 
 
 with open('newsafr2.txt', 'w', encoding='utf-8') as f:
-    f.write(get_top_10(root.findall('channel/item')))
+    f.write(get_top(root.findall('channel/item'), 6, 10))
